6a56fb397737c1ead3f7febee1aa994c141c1c58
aduylaai <aduylaai@gmail.com>
2025-07-01 10:25:36 +0700

    valid name and type in builder

--- Province_API/Core/Domain/AdministrativeAggregate/AdministrativeUnitBuilder.cs
+++ Province_API/Core/Domain/AdministrativeAggregate/AdministrativeUnitBuilder.cs
@@ -7,7 +7,7 @@ namespace Province_API.Core.Domain.AdministrativeAggregate
     public class AdministrativeUnitBuilder
     {
         public string name;
-        public AdministrativeUnitType type;
+        public AdministrativeUnitType? type;
         public string? parentid;
 
         public AdministrativeUnitBuilder SetName(string name)
@@ -30,7 +30,11 @@ namespace Province_API.Core.Domain.AdministrativeAggregate
 
         public AdminstrativeUnit Build()
         {
-           return new AdminstrativeUnit(this.name, this.type, this.parentid);
+       
+            var finalName = string.IsNullOrWhiteSpace(name) ? "Không rõ" : name;
+            var finalType = type ?? AdministrativeUnitType.Xa;
+          
+            return new AdminstrativeUnit(finalName, finalType, this.parentid);
         }
     }
 }
